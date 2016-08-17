7f2333bfab75f7596183064ad30352a0c04fce20
hmjyn <1277315305@qq.com>
2016-08-17 15:59:27 +0800

    Update fishman.py
    update to fishman

--- fishman.py
+++ fishman.py
@@ -1,1 +1,19 @@
-hello world!
+class fishman:
+    def __init__(self):
+        self.name=fishman
+        self.live=100
+        self.livestatus=1
+        self.fishnb=0
+        self.fishchick=0
+        self.fishislandnb=0
+    def fishhamter(self):
+        if random==1:
+            self.fishnb+=1
+
+    def fishtochick(self):
+        self.fishnb-=1
+        self.fishchick+=1
+
+    def foodtolive(self):
+        self.fishnb-=1
+        self.live+=50

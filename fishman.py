f65e4680a5ec98e9de3b66ae1e01ab0c20492135
hmjyn <1277315305@qq.com>
2016-08-18 18:09:35 +0800

    Update fishman.py
    v0.3.0

--- fishman.py
+++ fishman.py
@@ -1,15 +1,19 @@
+from random import randint
+
 class fishman:
     def __init__(self):
-        self.name=fishman
+        self.name="fishman"
         self.live=100
         self.livestatus=1
         self.fishnb=0
         self.fishchick=0
         self.fishislandnb=0
     def fishhamter(self):
-        if random==1:
+        #rrrdd=1
+        if randint(1,5) == 3:
             self.fishnb+=1
-
+        #print rrrdd
+        print self.fishnb
     def fishtochick(self):
         self.fishnb-=1
         self.fishchick+=1
@@ -17,3 +21,22 @@ class fishman:
     def foodtolive(self):
         self.fishnb-=1
         self.live+=50
+    def printall(self):
+        print self.name
+        print self.live
+        print self.livestatus
+        print self.fishnb
+        print self.fishchick
+        print self.fishislandnb
+
+if __
+
+
+  #      print "name="+self.name
+  #      print "live="+self.live
+  #      print "livestatus="+self.livestatus
+  #      print "fishnb="+self.fishnb
+  #      print "fishchick="+self.fishchick
+  #      print "fishislandnb="+self.fishislandnb
+
+  
